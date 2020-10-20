55d77b44e829762b9be38a1425efc0218d02f0c5
Pavithra <pavitn21@gmail.com>
2020-10-20 19:59:13 +0530

    Initial commit

---
+++ Assets/Scripts/link.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class link : MonoBehaviour
+{
+    public void radssoon()
+    {
+        Application.OpenURL("http://www.radssoon.com/");
+    }
+    public void youtube()
+    {
+        Application.OpenURL("https://youtu.be/0-RgS02kUsA");
+    }
+    public void facrbook()
+    {
+        Application.OpenURL("https://www.facebook.com/Radssoon/");
+    }
+    public void twitter()
+    {
+        Application.OpenURL("https://twitter.com/radssoon?lang=en");
+    }
+    public void call()
+    {
+        Application.OpenURL("tel://9999999999");
+    }
+
+}
